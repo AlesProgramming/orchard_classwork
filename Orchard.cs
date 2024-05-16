f2d6d784f228cd9d7b7b56c0610ab02ecb32dd7a
AlesProgramming <alesrhensel@gmail.com>
2024-05-16 09:35:16 +0200

    init

---
+++ Orchard.cs
@@ -0,0 +1,32 @@
+namespace Orchard;
+
+public class Orchard
+{
+    private int _length;
+    private int _width;
+    private string _plant;
+    private int _yieldMax;
+    private int _yieldMin;
+    
+    public Orchard(int length, int width, string plant, int yieldMax, int yieldMin)
+    {
+        _length = length;
+        _width = width;
+        _plant = plant;
+        _yieldMax = Math.Max(yieldMax, yieldMin);
+        _yieldMin = Math.Min(yieldMax, yieldMin);
+    }
+
+    public int Harvest()
+    {
+        Random rand = new Random();
+        int yield = rand.Next(_yieldMin, _yieldMax);
+        return yield * _length * _width;
+    }
+
+    public int Length => _length;
+    public int Width => _width;
+    public string Plant => _plant;
+    public int YieldMax => _yieldMax;
+    public int YieldMin => _yieldMin;
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Orchard {
+    public class Program {
+        public static void Main()
+        {
+            string[] crops = {"apple", "pear", "apricot", "cherry", "plum"};
+            Orchard[] orchards = new Orchard[20];
+
+            Dictionary<string, int> yields = new Dictionary<string, int>();
+
+            for (int x = 0; x < orchards.Length; x++)
+            {
+                Random rand = new Random();
+                Orchard temp = new Orchard(rand.Next(1, 10), rand.Next(1, 10), crops[rand.Next(0, 5)], rand.Next(1, 10), rand.Next(1, 10));
+                orchards[x] = temp;
+
+                if (yields.ContainsKey(temp.Plant))
+                {
+                    yields[temp.Plant] += temp.Harvest();
+                }
+                else
+                {
+                    yields.Add(temp.Plant, temp.Harvest());
+                }
+            }
+            
+            for (int x = 0; x < yields.Count; x++)
+            {
+                if (yields.ContainsKey(crops[x]))
+                {
+                    Console.WriteLine($"{crops[x]}: {yields[crops[x]]}kg");
+                }
+                else
+                {
+                    Console.WriteLine($"{crops[x]}: 0kg");
+                }
+            }
+
+        }
+    }
+}
